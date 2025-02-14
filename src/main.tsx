1909d612ccbfa4169759cbc1f7806fbff6147b02
armankhan <armaankhan813085@gmail.com>
2025-02-14 21:57:08 +0530

    Fixed GitHub Pages routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Import Router
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/tech-swai">
-        <App />
-    </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
